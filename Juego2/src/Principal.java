09465a7065e1f10682b861100f621138fdf7cdf0
Manu <Manu@DESKTOP-59E5QVC>
2020-04-12 21:22:23 +0200

    Implementando

--- Juego2/src/Principal.java
+++ Juego2/src/Principal.java
@@ -1,4 +1,23 @@
+import java.util.Scanner;
 
 public class Principal {
+	
+	public static void main(String[] args) throws Exception {
 
+		
+	String cadena=null;
+	Scanner sc = new Scanner(System.in); 
+	
+	while (cadena!="salir") {
+
+	cadena=sc.nextLine();
+	StringBuilder builder=new StringBuilder(cadena);	
+	String CadenaInvertida=null;
+	CadenaInvertida=builder.reverse().toString();
+	
+	}
+	sc.close();
+	}
+
+	
 }
